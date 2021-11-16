29d346e0a64e6fa9face151a24012dbfcf2a0291
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-11-17 01:23:28 +0300

    Добавьте файлы проекта.

---
+++ TrainingPortalWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TrainingPortalWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
