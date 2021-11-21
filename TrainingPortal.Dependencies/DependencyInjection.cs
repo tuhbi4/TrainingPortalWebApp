47cef61ecdf4e1d67368bcc0756ac45a7ba0298c
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-11-22 00:48:57 +0300

    add DI

---
+++ TrainingPortal.Dependencies/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using TrainingPortal.BLL.Interfaces;
+using TrainingPortal.BLL.Services;
+using TrainingPortal.DAL.Interfaces;
+using TrainingPortal.DAL.Mocks;
+using TrainingPortal.Entities;
+
+namespace TrainingPortal.Dependencies
+{
+    public static class DependencyInjection
+    {
+        public static void InjectDependencies(this IServiceCollection services)
+        {
+            services.AddSingleton<IRepository<Category>, MockRepository<Category>>();
+            services.AddSingleton<IRepositoryService<Category>, RepositoryService<Category>>();
+            services.AddSingleton<IRepository<Course>, MockRepository<Course>>();
+            services.AddSingleton<IRepositoryService<Course>, RepositoryService<Course>>();
+        }
+    }
+}
