d56f3276ff9f63a01b79012a8973d6916f3e0484
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-11-22 00:48:05 +0300

    add repository class + interface

---
+++ TrainingPortal.DAL.Interfaces/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace TrainingPortal.DAL.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        public List<T> Items { get; }
+
+        public void Create(T dataInstance);
+
+        public T Read(int id);
+
+        public List<T> ReadAll();
+
+        public void Update(int id, T dataInstance);
+
+        public void Delete(int id);
+    }
+}
