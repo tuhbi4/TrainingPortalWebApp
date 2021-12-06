854efac9a9312aefcbbb01d06813257c6898b1e2
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-12-06 05:18:35 +0300

    add bll interfaces

---
+++ TrainingPortal.BLL.Interfaces/IModelMapper.cs
@@ -0,0 +1,9 @@
+﻿namespace TrainingPortal.BLL.Interfaces
+{
+    public interface IModelMapper
+    {
+        public TOut ConvertToDomainModel<TIn, TOut>(TIn sourceInstance) where TIn : class where TOut : class;
+
+        public TOut ConvertToDboModel<TIn, TOut>(TIn sourceInstance) where TIn : class where TOut : class;
+    }
+}
