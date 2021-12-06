453f9af86801e4de580a654f9170f6a7757b3859
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-12-06 05:14:15 +0300

    add dal interfaces

--- TrainingPortal.DAL.Interfaces/IDboRepository.cs
+++ TrainingPortal.DAL.Interfaces/IDboRepository.cs
@@ -2,18 +2,18 @@
 
 namespace TrainingPortal.DAL.Interfaces
 {
-    public interface IRepository<T> where T : class
+    public interface IDboRepository<T> where T : class
     {
         public List<T> Items { get; }
 
-        public void Create(T dataInstance);
+        public int Create(T dataInstance);
 
         public T Read(int id);
 
         public List<T> ReadAll();
 
-        public void Update(int id, T dataInstance);
+        public int Update(int id, T dataInstance);
 
-        public void Delete(int id);
+        public int Delete(int id);
     }
 }
