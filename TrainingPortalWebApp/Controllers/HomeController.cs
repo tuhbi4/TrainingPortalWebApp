3942435dcee8686579df2c4381646c7b8b56401d
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2021-12-11 06:07:54 +0300

    add accessDinien view

--- TrainingPortalWebApp/Controllers/HomeController.cs
+++ TrainingPortalWebApp/Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace TrainingPortal.WebPL.Controllers
         {
             return View();
         }
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
