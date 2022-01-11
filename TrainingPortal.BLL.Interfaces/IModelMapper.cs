bd0458ecf9daf4e99809770f196cba8d51b6925d
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2022-01-11 11:06:05 +0300

    delete custom mappers

--- TrainingPortal.BLL.Interfaces/IModelMapper.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrainingPortal.BLL.Interfaces
-{
-    public interface IModelMapper
-    {
-        public TOut ConvertToDomainModel<TIn, TOut>(TIn sourceInstance) where TIn : class where TOut : class;
-
-        public TOut ConvertToDboModel<TIn, TOut>(TIn sourceInstance) where TIn : class where TOut : class;
-    }
-}
