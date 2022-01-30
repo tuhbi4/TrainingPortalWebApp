c85833a3f26e08f0c2ff88221423fe1d09892568
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2022-01-31 02:34:09 +0300

    fix model + move to own folder

---
+++ TrainingPortalWebApp/Models/Course/CourseViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using TrainingPortal.BLL.Models;
+using TrainingPortal.BLL.Models.CourseItems;
+
+namespace TrainingPortal.WebPL.Models.Course
+{
+    public class CourseViewModel
+    {
+        [Required]
+        [MaxLength(500)]
+        public string Name { get; set; }
+
+        [Required]
+        [MaxLength(1000)]
+        public string Description { get; set; }
+
+        public Category Category { get; set; }
+
+        public List<Lesson> LessonsList { get; set; }
+
+        [Required]
+        public Test Test { get; set; }
+
+        [Required]
+        public Certificate Certificate { get; set; }
+
+        public List<TargetAudience> TargetAudienciesList { get; set; }
+    }
+}
