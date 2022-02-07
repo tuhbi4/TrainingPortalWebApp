98140c54eeebc74c9a0ebdeec259cc568ee5d45f
tuhbi4 <52888514+tuhbi4@users.noreply.github.com>
2022-02-07 03:40:25 +0300

    add models

---
+++ TrainingPortal.DAL.Dbo/Models/UserPassedCourseDboRelation.cs
@@ -0,0 +1,18 @@
+﻿namespace TrainingPortal.DAL.Dbo.Models
+{
+    public class UserPassedCourseDboRelation
+    {
+        public int UserId { get; set; }
+
+        public int CourseId { get; set; }
+
+        public UserPassedCourseDboRelation()
+        { }
+
+        public UserPassedCourseDboRelation(int userId, int courseId)
+        {
+            UserId = userId;
+            CourseId = courseId;
+        }
+    }
+}
